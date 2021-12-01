657785271c0f7d5b75e13df953874e5a29b0e2be
chriswasser <christian_wassermann@web.de>
2021-12-01 20:57:36 +0100

    Add improved solution for Day01

--- Day01/main.py
+++ Day01/main.py
@@ -2,33 +2,38 @@
 
 import fileinput
 
+
+def num_increases(numbers, offset=1):
+    increases = 0
+    for a, b in zip(numbers[:-offset], numbers[offset:]):
+        if b > a:
+            increases += 1
+    return increases
+
+
 def test_task1():
-    assert True
+    numbers = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    solution = num_increases(numbers)
+    assert solution == 7
     print("tests for task 1: ok")
 
 
 def solve_task1():
     numbers = [int(line.rstrip()) for line in fileinput.input()]
-    increases = 0
-    for a, b in zip(numbers[:-1], numbers[1:]):
-        if b > a:
-            increases += 1
-    solution = increases
+    solution = num_increases(numbers)
     print(f"answer to task 1: {solution}")
 
 
 def test_task2():
-    assert True
+    numbers = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    solution = num_increases(numbers, offset=3)
+    assert solution == 5
     print("tests for task 2: ok")
 
 
 def solve_task2():
     numbers = [int(line.rstrip()) for line in fileinput.input()]
-    increases = 0
-    for a, b in zip(numbers[:-3], numbers[3:]):
-        if b > a:
-            increases += 1
-    solution = increases
+    solution = num_increases(numbers, offset=3)
     print(f"answer to task 2: {solution}")
 
 
